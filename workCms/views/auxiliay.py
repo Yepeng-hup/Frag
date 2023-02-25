31d72f695209c9fb4bd9488c6b5189e6a1e1babf
ye <ye@15888520997@163.com>
2023-02-25 14:26:55 +0800

    #

--- workCms/views/auxiliay.py
+++ workCms/views/auxiliay.py
@@ -4,12 +4,6 @@ import time
 dates = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 def get_size(bytes, suffix="B"):
-    """
-    Scale bytes to its proper format
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-    """
     factor = 1024
     for unit in ["", "K", "M", "G", "T", "P"]:
         if bytes < factor:
